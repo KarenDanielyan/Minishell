a041221b75d0a0001dd639574b6fc6b97c266632
KarenDanielyan <karendanielyan66@gmail.com>
2023-05-28 14:42:36 +0400

    history moved to BuiltIns

--- include/minishell.h
+++ include/minishell.h
@@ -6,21 +6,21 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:00:51 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/05/27 16:27:34 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/05/28 14:32:03 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "defines.h"
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <libft.h>
+# include <stdint.h>
 # include <stdio.h>
-# include <libft.h>
-# include <stdlib.h>
 # include <fcntl.h>
+# include <libft.h>
 
-void	true_loop(void);
+void	history(void);
 
 #endif
---
+++ src/BuiltIns/history.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   history.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 14:30:44 by kdaniely          #+#    #+#             */
+/*   Updated: 2023/05/28 14:31:01 by kdaniely         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * @brief	showing history
+ * 
+ * 			History builtin prints on the screen the contents
+ * 			of the history file.
+ */
+void	history(void)
+{
+	int		i;
+	int		fd;
+	char	*line;
+	char	*filename;
+
+	i = 1;
+	line = NULL;
+	filename = ft_strjoin(getenv("HOME"), HIST_FILE);
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		printf ("%d	%s", i++, line);
+		free(line);
+		line = NULL;
+	}
+	close(fd);
+}
