1719c8a09d3d9a6df352214d532bdfce11579af9
KarenDanielyan <karendanielyan66@gmail.com>
2023-05-28 14:41:19 +0400

    Code Refactoring

--- src/main.c
+++ src/main.c
@@ -6,11 +6,37 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 17:18:19 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/05/27 16:27:20 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/05/28 14:31:43 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief		Readline while loop
+ * 
+ */
+void	true_loop(void)
+{
+	int		fd;
+	char	*str;
+	char	*filename;
+
+	filename = ft_strjoin(getenv("HOME"), HIST_FILE);
+	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 0666);
+	free(filename);
+	while (1)
+	{
+		str = readline("minishell-3.2$ ");
+		if (ft_strlen(str) != 0)
+		{
+			add_history(str);
+			ft_putendl_fd(str, fd);
+			if (ft_strcmp(str, "history") == 0)
+				history();
+		}
+	}
+}
 
 int	main(int ac, char **av, char **env)
 {
