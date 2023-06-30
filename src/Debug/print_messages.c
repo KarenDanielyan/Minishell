9c68f24350e3d40989e6f7fb735b4e1dd98372ff
KarenDanielyan <karendanielyan66@gmail.com>
2023-06-30 18:55:38 +0400

    depricate print_flags

--- src/Debug/print_messages.c
+++ src/Debug/print_messages.c
@@ -6,19 +6,11 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 14:59:08 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/06/30 17:28:28 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/06/30 18:54:37 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
-
-void	print_flags(int flags)
-{
-	printf("| HASDOLLAR | SQUOTED | DQUOTE | SUBSHELL_PIPE | SUBSHELL_PAREN |\n");
-	printf("|      %d    |    %d    |    %d   |       %d       |         %d      |\n",
-		((flags & W_HASDOLLAR) != 0), ((flags & W_SQUOTE) != 0), ((flags & W_DQUOTE) != 0), \
-		((flags & W_SUBSHELL_PIPE) != 0), ((flags & W_SUBSHELL_PAREN) != 0));
-}
 
 void	print_wordl(t_wordl *wordl)
 {
@@ -28,7 +20,7 @@ void	print_wordl(t_wordl *wordl)
 		printf("\033[32m{%s}\033[0m → ", wordl->word->value);
 		wordl = wordl->next;
 	}
-	printf("\n");
+	printf("NULL\n");
 }
 
 void	print_tokens(t_token	*token_list)
@@ -36,10 +28,10 @@ void	print_tokens(t_token	*token_list)
 	while (token_list)
 	{
 		printf("%p\n", token_list);
-		print_flags(token_list->wordl->word->flags);
 		printf("\033[34mToken Type:	\033[32m%s\n\033[0m", get_type(token_list->type));
 		print_wordl(token_list->wordl);
 		printf("↓\n");
 		token_list = token_list->next;
 	}
+	printf("NULL\n");
 }
