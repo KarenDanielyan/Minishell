0a5981e0161bf73104b82aedd33ccf54f35efc12
KarenDanielyan <karendanielyan66@gmail.com>
2023-07-09 18:14:00 +0400

    HOTFIX: quote_removal()

--- src/Expansions/quote_removal.c
+++ src/Expansions/quote_removal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quote_removal.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 15:33:30 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/07/09 17:35:26 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/07/09 18:10:12 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,11 @@ void	quote_removal(t_node *self)
 			tmp = ft_strtrim(temp->word->value, DQUOTE_S);
 		if (temp->word->flags & W_SQUOTE)
 			tmp = ft_strtrim(temp->word->value, SQUOTE_S);
-		free(temp->word->value);
-		temp->word->value = tmp;
+		if (tmp != NULL)
+		{
+			free(temp->word->value);
+			temp->word->value = tmp;
+		}
 		temp = temp->next;
 	}
 }
