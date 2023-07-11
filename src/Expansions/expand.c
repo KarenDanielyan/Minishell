4bd9fdfbd0585b867bc8c2c6915ccd1c418ce513
KarenDanielyan <karendanielyan66@gmail.com>
2023-07-11 19:46:34 +0400

    Preprocessing p.1

--- src/Expansions/expand.c
+++ src/Expansions/expand.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 02:55:38 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/07/11 01:26:14 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/07/11 18:55:23 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void	expand(t_control *ctl, t_node *self)
 	{
 		//tilde_exp(self, ctl->var_list);
 		param_exp(self, ctl->var_list);
-		//field_splitting(self, ctl->var_list);
+		field_splitting(self, ctl->var_list);
 		//glob_exp(self);
-		//quote_removal(self);
-		//self->value.word = make_word(self->value.word);
+		quote_removal(self);
+		self->value.word = make_word(self->value.word);
 	}
 	return ;
 }
