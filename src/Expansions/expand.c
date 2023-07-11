5847bb5bb90bc36b2d80b9621b077bbcf2a4b7c3
David_Ohanyan <davitoahnyan630@gmail.com>
2023-07-11 19:21:14 +0400

    if_pattern_in_couts

--- src/Expansions/expand.c
+++ src/Expansions/expand.c
@@ -6,7 +6,7 @@
 /*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 02:55:38 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/07/10 21:22:58 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/07/11 15:44:41 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	expand(t_control *ctl, t_node *self)
 		field_splitting(self, ctl->var_list);
 		glob_exp(self);
 		quote_removal(self);
-		// self->value.word = make_word(self->value.word);
+		self->value.word = make_word(self->value.word);
 	}
 	return ;
 }
--- src/Expansions/glob_exp.c
+++ src/Expansions/glob_exp.c
@@ -6,7 +6,7 @@
 /*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 22:37:10 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/07/10 21:19:03 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/07/11 18:09:52 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,39 @@
 #include <dirent.h>
 #include <stdio.h>
 
+int if_pattern_in_couts(t_wordl *args)
+{
+	int flag;
+
+	flag = 0;
+	while (args)
+	{
+		if(ft_strchr(args->word->value, '*'))
+		{
+			flag = 1;
+			if ((args->word->flags & W_DQUOTE) || (args->word->flags & W_SQUOTE))
+				return(0);
+		}
+		args = args->next;
+	}
+	if (!flag)
+		return (0);
+	return (1);
+}
+
 void	glob_exp(t_node *node)
-{	DIR *dir;
-	char **split;
+{
+	// DIR *dir;
+	// char **split;
 	char *str;
 	t_wordl *args;
-	struct dirent *temp;
+	// struct dirent *temp;
 	str = "";
 	
 	args =  node->value.word;
-	while ()
+	if (!if_pattern_in_couts(args))
+		return ;
+	while (args)
 	{
 		
 	}
@@ -36,5 +59,5 @@ void	glob_exp(t_node *node)
 	// temp = readdir(dir);
 	// temp = readdir(dir);
 	// temp = readdir(dir);
-        printf("%s\n", temp->d_name);
+        // printf("%s\n", temp->d_name);
 }
