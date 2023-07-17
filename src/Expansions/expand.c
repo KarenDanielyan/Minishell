48ff07445d7b14557d8e77da348528e17f861c8b
KarenDanielyan <karendanielyan66@gmail.com>
2023-07-17 16:53:09 +0400

    New expand 99%

--- src/Expansions/expand.c
+++ src/Expansions/expand.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 02:55:38 by kdaniely          #+#    #+#             */
-/*   Updated: 2023/07/17 01:26:42 by kdaniely         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:18:13 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	expand(t_control *ctl, t_node *self)
 		tilde_exp(self, ctl->var_list);
 		param_exp(self, ctl->var_list);
 		field_splitting(self, ctl->var_list);
+		self->value.word = make_word(self->value.word);
 		//glob_exp(self);
 		quote_removal(self);
-		self->value.word = make_word(self->value.word);
 	}
 	return ;
 }
